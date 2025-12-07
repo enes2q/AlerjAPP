76892670b9ffe798f3cfe075dce789ce5cb0dd3f
ipekyukseell <ipekyuksel68@icloud.com>
2025-12-07 16:37:46 +0300

    Update urun_data.py

---
+++ urun_data.py
@@ -0,0 +1,59 @@
+# urun_data.py
+# Projedeki tüm ürünler ve içindekiler listesi burada tutuluyor.
+
+urunler = {
+    "Ulker Cikolatalı Gofret": "Şeker, süt tozu, fındık, kakao kitlesi, buğday unu (gluten), bitkisel yağ, emülgatör (lesitin), E330",
+
+    "Eti Form Kepekli Biskuvi": "Tam buğday unu (gluten), bitkisel yağ, şeker, tuz, kabartıcılar (E500, E503), koruyucu (E202)",
+
+    "Coca-Cola Zero": "Karbonatlı su, renklendirici (E150d), tatlandırıcı (E951), asit düzenleyici (E338), aroma vericiler",
+
+    "Fanta Portakal": "Karbonatlı su, şeker, asitlik düzenleyici (E330), renklendirici, doğal aroma, stabilizör (E414)",
+
+    "Red Bull Enerji Icecegi": "Su, sakaroz, glukoz, asitlik düzenleyici (sitrik asit), taurin, aroma vericiler, vitaminler (B3, B5, B6, B12), kafein",
+
+    "Nestle Damak": "Şeker, fıstık, kakao yağı, kakao kitlesi, süt tozu, emülgatör (lesitin)",
+
+    "Ulker Biskrem": "Buğday unu (gluten), şeker, bitkisel yağ, kakao tozu, glikoz şurubu, süt tozu, kabartıcılar (E500, E503)",
+
+    "Eti Tutku": "Buğday unu (gluten), şeker, kakao tozu, bitkisel yağ, glikoz-fruktoz şurubu, süt tozu, kabartıcı (E500)",
+
+    "Nescafe 3u1 Arada": "Şeker, kahve kreması (süt türevi), çözünebilir kahve, aroma vericiler",
+
+    "Cappy Portakal Suyu": "Su, portakal suyu konsantresi, şeker, asitlik düzenleyici (E330), aroma",
+
+    "Pınar Süt Tam Yağlı": "İnek sütü, vitamin D",
+
+    "Sırma Soda Limon": "Doğal mineralli su, karbondioksit, limon aroması",
+
+    "Lay's Klasik Patates Cipsi": "Patates, ayçiçek yağı, tuz",
+
+    "Doritos Nacho": "Mısır unu, bitkisel yağ, peynir aroması, baharat karışımı, tuz, renklendirici (E160b)",
+
+    "Ruffles Original": "Patates, ayçiçek yağı, tuz",
+
+    "Tadım Kavrulmuş Fındık": "Fındık",
+
+    "Ülker Hanımeller": "Buğday unu (gluten), şeker, bitkisel yağ, süt tozu, glikoz şurubu, kabartıcı (E500)",
+
+    "Eti Browni Gold": "Şeker, buğday unu (gluten), bitkisel yağ, kakao, glikoz şurubu, kabartıcı (E500), süt tozu",
+
+    "Coca-Cola Classic": "Karbonatlı su, şeker, renklendirici (E150d), asit düzenleyici (E338), aroma vericiler, kafein",
+
+    "Fuse Tea Şeftali": "Su, şeker, siyah çay ekstraktı, aroma vericiler, asit düzenleyici (E330)",
+
+    "Mis Süt Laktozsuz": "İnek sütü, laktaz enzimi, vitamin D",
+
+    "Kinder Bueno": "Sütlü krema, şeker, buğday unu (gluten), bitkisel yağ, fındık, kakao, lesitin (E322)",
+
+    "Snickers Bar": "Şeker, yer fıstığı, glikoz şurubu, süt tozu, kakao yağı, kakao kitlesi, tuz, emülgatör (E476)",
+
+    "İçim Yoğurt": "Pastörize inek sütü, yoğurt kültürü",
+
+    "Torku Banada": "Şeker, bitkisel yağ, fındık, kakao tozu, süt tozu, emülgatör (lesitin)",
+
+    "Nestle Corn Flakes": "Mısır, şeker, arpa malt özü (gluten), tuz, vitamin ve mineraller",
+
+    "Nescafe Gold": "100% çözünebilir kahve",
+
+    "Lipton Ice Tea Limon": "Su, şeker,
